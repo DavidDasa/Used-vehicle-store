58986bdeed45e9372e8aa17cee5e341d7184a752
DavidDasa <88465622+DavidDasa@users.noreply.github.com>
2022-07-05 10:17:57 +0300

    Add files via upload

---
+++ Yad2_Cars/ElectricCar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yad2_Cars
+{
+    [Serializable]
+    class ElectricCar : Car
+    {
+        public int _range { get; set; }
+        public double _chargeTime { get; set; }
+
+        public ElectricCar(int Range, double ChargeTime, bool IsSedan, int NumSeats, int Mileage, int Year, string Model, int Price, int Hand = 2) : base(IsSedan, NumSeats, Mileage, Year, Model, Price, Hand)
+        {
+            _range = Range;
+            _chargeTime = ChargeTime;
+        }
+        public override string GiveInfo()
+        {
+            string info;
+            string sedan;
+            if (_isSedan)
+                sedan = "it's a sedan.";
+            else
+                sedan = "it's not a sedan.";
+
+            info = $"This is a {_year} {_model}, it has {_mileage}KM ,it's {_hand} hand with {_numSeats} seats in it, and {sedan} " +
+                $"Also it's capable of traveling {_range} with a single charge and a charging time of {_chargeTime} hours.\nPrice:{_price}₪";
+            return info;
+        }
+    }
+}
